5c502e8690cb3c974828963654da0fcf212e5555
tuphan <tuphan@descartes.network>
2021-07-02 16:49:02 +0700

    search engine

---
+++ src/containers/market/search/engine.js
@@ -0,0 +1,43 @@
+import { Document } from 'flexsearch';
+
+const PRESET = {
+  tokenize: 'full',
+  context: true,
+  minlength: 3
+}
+
+class SearchEngine {
+  constructor(universe) {
+    this.universe = universe;
+    this.index = new Document({
+      document: {
+        id: 'id',
+        index: [
+          { field: 'name', ...PRESET },
+          { field: 'description', ...PRESET },
+          { field: 'keywords', ...PRESET },
+          { field: 'author:name', ...PRESET },
+          { field: 'author:email', ...PRESET },
+        ]
+      }
+    });
+    Object.keys(universe).forEach(id => this.add({ id, ...universe[id] }));
+  }
+
+  add = (id, doc) => {
+    return this.index.add(id, doc);
+  }
+
+  search = (text, limit = 5) => {
+    const raw = this.index.search(text, limit);
+    let ids = [];
+    raw.forEach(({ result }) => {
+      return result.forEach(id => {
+        if (!ids.includes(id)) return ids.push(id);
+      });
+    });
+    return ids.map(id => this.universe[id].name);
+  }
+}
+
+export default SearchEngine;
--- src/containers/market/search/index.js
+++ src/containers/market/search/index.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from '@reduxjs/toolkit';
 import { withRouter } from 'react-router-dom';
+import universe from 'universe.json';
 
 import { Row, Col, Card, Input, Icon, Button, Typography, Space } from 'sen-kit';
 
+import { DynamicLogo } from 'helpers/loader';
 import Keyword from './keyword';
+import SearchEngine from './engine';
 
 
 class Search extends Component {
@@ -13,45 +16,63 @@ class Search extends Component {
     super();
 
     this.state = {
-      keywords: ""
+      loading: false,
+      keywords: '',
+      appNames: []
     }
+
+    this.engine = new SearchEngine(universe);
   }
 
-  onKeywords = (e) => {
+  onSearch = (e) => {
     const keywords = e.target.value;
-    return this.setState({ keywords });
-  }
-
-  onSearch = () => {
-    const { keywords } = this.state;
-    console.log(keywords);
+    return this.setState({ keywords, loading: true }, () => {
+      clearTimeout(this.searching);
+      this.searching = setTimeout(() => {
+        const appNames = this.engine.search(keywords);
+        return this.setState({ appNames, loading: false });
+      }, 1500);
+    });
   }
 
   render() {
-    const { keywords } = this.state;
+    const { loading, keywords, appNames } = this.state;
 
     return <Row gutter={[16, 16]} justify="center">
-      <Col span={20}>
+      <Col span={24}>
         <Card bordered={false} bodyStyle={{ padding: 8 }}>
           <Row gutter={[8, 8]} justify="end">
             <Col span={24}>
               <Input
                 placeholder="keywords"
                 value={keywords}
-                suffix={<Button
+                suffix={keywords ? <Button
+                  type="text"
+                  style={{ marginLeft: -7 }}
+                  icon={<Icon name="close-circle-outline" />}
+                  loading={loading}
+                  onClick={() => this.onSearch({ target: { value: '' } })}
+                /> : <Button
                   type="text"
                   style={{ marginRight: -7 }}
                   icon={<Icon name="search-outline" />}
-                  onClick={this.onSearch}
+                  loading={loading}
                 />}
-                onChange={this.onKeywords}
-                onPressEnter={this.onSearch}
+                onChange={this.onSearch}
               />
             </Col>
             <Col>
               <Space>
                 <Typography.Text>Popular:</Typography.Text>
-                <Keyword title="swap" />
+                <Keyword
+                  title="swap"
+                  onClick={() => this.onSearch({ target: { value: 'swap' } })}
+                />
+              </Space>
+            </Col>
+            <Col span={24}>
+              <Space align="start">
+                {appNames.map(appName => <DynamicLogo key={appName} name={appName} />)}
               </Space>
             </Col>
           </Row>
