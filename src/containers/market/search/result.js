a0fdf8794a49b5406f139aa2cfa3a22619744607
tuphan <tuphan@descartes.network>
2021-07-06 20:55:17 +0700

    searchbar

--- src/containers/market/search/result.js
+++ src/containers/market/search/result.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Row, Col, Typography } from 'sen-kit';
+import { Row, Col, Typography, Button, Icon, Space } from 'sen-kit';
 
 import metadata, { DynamicPanel } from 'helpers/loader';
 
 const Result = ({ appName, onClick }) => {
   const { description } = metadata(appName);
   const typoMargin = { margin: '4px 8px' }
-  return <Row gutter={[8, 8]} style={{ cursor: 'pointer' }} onClick={onClick}>
-    <Col span={24}>
+  return <Row gutter={[8, 8]}>
+    <Col span={24} className="result-card" onClick={onClick}>
       <DynamicPanel appName={appName} />
+      <div className="result-card-mask">
+        <Row gutter={[8, 8]} align="middle" justify="center">
+          <Col>
+            <Space>
+              <Button
+                type="text"
+                size="large"
+                shape="circle"
+                icon={<Icon name="eye-outline" />}
+              />
+            </Space>
+          </Col>
+        </Row>
+      </div>
     </Col>
     <Col span={24}>
       <Typography.Title level={5} style={typoMargin}>{appName}</Typography.Title>
