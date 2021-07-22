a5756f4f5c052351ceea85b9325e76ccd0300049
tuphan-dn <tuphan@descartes.network>
2021-07-22 18:43:03 +0700

    update preset

--- src/containers/utility/preset/intro.tsx
+++ src/containers/utility/preset/intro.tsx
@@ -15,25 +15,31 @@ const Intro = ({
   onClick: () => void
 }) => {
   return (
-    <Card bordered={selected} hoverable>
-      <Row gutter={[16, 16]} onClick={onClick}>
-        <Col span={24}>
-          <Typography.Title level={5} style={{ margin: 0 }}>
-            {type}
-          </Typography.Title>
-          <Typography.Text type="secondary">{description}</Typography.Text>
-        </Col>
-        <Col span={24}>
-          <Row gutter={[16, 8]}>
-            {preset.map((appName) => (
-              <Col key={appName}>
-                <DynamicLogo name={appName} />
-              </Col>
-            ))}
+    <Row gutter={[16, 16]} onClick={onClick}>
+      <Col span={24}>
+        <Typography.Title level={5} style={{ margin: 0 }}>
+          {type}
+        </Typography.Title>
+        <Typography.Text type="secondary" style={{ fontSize: 11 }}>
+          {description}
+        </Typography.Text>
+      </Col>
+      <Col span={24}>
+        <Card bordered={selected} bodyStyle={{ padding: 16 }} hoverable>
+          <Row gutter={[16, 16]} onClick={onClick}>
+            <Col span={24}>
+              <Row gutter={[16, 8]}>
+                {preset.map((appName) => (
+                  <Col key={appName}>
+                    <DynamicLogo name={appName} />
+                  </Col>
+                ))}
+              </Row>
+            </Col>
           </Row>
-        </Col>
-      </Row>
-    </Card>
+        </Card>
+      </Col>
+    </Row>
   )
 }
 
